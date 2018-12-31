09cad4904451b91b7438ff0ca88940502a2752fd
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2018-12-31 17:37:48 +0530

    Update ready.js
    added events in the console section and remove the dbl thingy

--- events/ready.js
+++ events/ready.js
@@ -2,17 +2,13 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const chalk = require("chalk");
 const botconfig = require("../botconfig.json");
-const Dbl = require("dblapi.js");
 module.exports = async (bot) => {
-  const dbl = new Dbl(botconfig.dbl, bot);
-    
         setGame();
-        console.log(chalk.blue(`${bot.user.username} has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds and ${bot.commands.size} commands!`)); 
+        console.log(chalk.blue(`${bot.user.username} has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds, ${bot.commands.size} commands! and ${bot.eventNames().length -2} events`)); 
         console.log(chalk.green(`Sparky Version: ${botconfig.version}`));
         console.log(chalk.blue(`Sparky Developers:${botconfig.devs}`));
         console.log(chalk.green(`Sparky Prefix: ${botconfig.PREFIX}`));
-dbl.postStats(bot.guilds.size);
-    function setGame() {
+       function setGame() {
         const set = () => {
             bot.user.setActivity(`for ${botconfig.PREFIX}help ${bot.guilds.size} Guilds and ${bot.users.size} Users`, {type:botconfig.stats}).catch(err => console.log(err));
         };
