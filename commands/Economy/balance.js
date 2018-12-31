6f8fc6d6b3639ceabfaadc7c15a1f69a2730ecb7
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2018-12-31 17:19:48 +0530

    fixed some lol stuff

--- commands/Economy/balance.js
+++ commands/Economy/balance.js
@@ -1,14 +1,9 @@
 const Discord = require("discord.js");
 const eco = require('discord-economy');
 
-module.exports.run = async (bot, message) => {
-    const Discord = require("discord.js");
-    const eco = require('discord-economy');
-    
-    module.exports.run = async (bot, message) => {
+  module.exports.run = async (bot, message) => {
         var output = await eco.FetchBalance(message.author.id)
         message.channel.send(`Hey ${message.author.tag}! You own ${output.balance} coins.`);
-};
 };
 
 module.exports.command = {
