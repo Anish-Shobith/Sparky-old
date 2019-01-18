e1d6194b59aa035985281d67f23d8361d40651b0
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2019-01-19 02:56:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const { Client } = require("discord.js");
 const Sparky = new Client();
 
+Sparky.on("error", async (e) => {
+console.log(e)
+})
+
+Sparky.on("ready", async () => {
+console.log("I Am Ready!!!!")
+})
 
 Sparky.login(process.env.token)
